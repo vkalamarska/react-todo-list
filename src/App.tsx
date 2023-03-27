261a5b7b0a4c03371c5dc5b63e99f5b3b1fadc41
Viktoriia Kalamarska <vkalamarska@gmail.com>
2023-03-27 17:43:12 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const AppWrapper = styled.section`
+  width: 100%;
+  height: 100%;
+  background-color: #f3f4f5;
+`;
+
+function App() {
+  return <AppWrapper>hgfdhdfhgf</AppWrapper>;
+}
+
+export default App;
